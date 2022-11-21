4089d9c44543b3240832c348bda23047b59a2757
unknown <Admin@OE-NUMOA-DADOS.semsa.am.br>
2022-11-21 14:45:23 -0400

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req,res)=> {
     res.send('<h1>Welcome to my webpage Oberdan!</h1>')
 })
+
+app.listen(PORT, ()=> console.log('app running at port: ', PORT))
 
